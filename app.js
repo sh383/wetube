0377b8016230932e8b05f8a6a071376cfd3bd9ce
Sanghoon Shin <sanghoonshin@Sanghoonui-MacBook-Pro.local>
2020-06-23 00:41:02 +0900

    pug, header, footer

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
-const app = express()
+const app = express();
 
+app.use(helmet());
+app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
-app.use(helmet());
 app.use(morgan('dev'));
+
+//미들웨어, 파일로 따로 분리
+//맨 위에 놓음으로써 아래 코드들이 미들웨어, local 에 접근할 수 있도록
+app.use(localsMiddleware);
 
 //가입, 검색, 홈 등등
 app.use(routes.home, globalRouter);
---
+++ middlewares.js
@@ -0,0 +1,10 @@
+import routes from "./routes";
+
+export const localsMiddleware = (req,res,next) => {
+    //siteName 은 임의의 이름
+    res.locals.siteName = 'WeTube';
+    
+    //로컬로 등록해놔서 routes 를 아무데서나(global) 변수처럼 사용 가능. 사용자 정보를 저장하는 방법이 있음.
+    res.locals.routes = routes;
+    next();
+};
