4301f718821c315d74b34cf1bf53861eb9734e7c
Sanghoon Shin <sanghoonshin@Sanghoonui-MacBook-Pro.local>
2020-07-06 21:35:44 +0900

    dotenv

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,8 +10,8 @@ import {
 const userRouter = express.Router();
 
 //userDetail 함수 선언
-userRouter.get(routes.userDetail(), userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail(), userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
