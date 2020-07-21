fea13a9e87dd4c9c841fbaa82015932e6c99f605
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-07-21 18:55:37 +0900

    #5.1 Footer, Login & Join

--- middlewares.js
+++ middlewares.js
@@ -4,17 +4,17 @@ import routes from "./routes";
 // 만약 /uploads/videos/  이런식으로 쓰면 프로젝트 폴더가 아니라 컴퓨터의 root 폴더를 기준으로 저장
 const multerVideo = multer({ dest: "uploads/videos/" });
 
-export const localsMiddleware = (req,res,next) => {
-    //siteName 은 임의의 이름
-    res.locals.siteName = 'WeTube';
-    
-    //로컬로 등록해놔서 routes 를 아무데서나(global) 변수처럼 사용 가능. 사용자 정보를 저장하는 방법이 있음.
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+export const localsMiddleware = (req, res, next) => {
+  //siteName 은 임의의 이름
+  res.locals.siteName = "WeTube";
+
+  //로컬로 등록해놔서 routes 를 아무데서나(global) 변수처럼 사용 가능. 사용자 정보를 저장하는 방법이 있음.
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  next();
 };
 // single 은 한 파일만 올리도록
 export const uploadVideo = multerVideo.single("videoFile");
