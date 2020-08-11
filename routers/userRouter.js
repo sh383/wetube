b10b2972f52cfddc0a841b3ae8da541499b21ca0
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-08-10 20:29:48 -0500

    #7.0 User Profile

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,15 +2,15 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  editProfile,
   changePassword,
+  getEditProfile,
 } from "../controllers/userController";
 import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
 //userDetail 함수 선언
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.userDetail(), onlyPrivate, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
