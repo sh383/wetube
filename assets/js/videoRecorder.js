7cf2c954f4683f7c4eaea58920f09a5c6b7d52bb
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-08-20 18:09:28 -0500

    #9.2 Recording video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,15 +3,29 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = (event) => {
-  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click(); // faking click
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start Recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
-  videoRecorder.start();
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start(); // 덩어리 단위로 녹화 videoRecorder.start(1000);
   videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
@@ -24,7 +38,7 @@ const getVideo = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
-    recordBtn.innerHTML = "Stop recording";
+    recordBtn.innerHTML = "Stop Recording";
     streamObject = stream;
     startRecording();
   } catch (error) {
